f283020c2b9e137abf7cb6a52b7df52c7fa87049
Moaz Elbeshbeshy <awladnasem800@gmail.com>
2025-05-06 11:43:44 +0200

    added error classes, added jwt to encrypt the username,id

---
+++ errors/index.js
@@ -0,0 +1,13 @@
+const BadRequest = require('./bad-request')
+const UnauthenticatedError = require('./unauthenticated')
+const CustomAPIError = require('./custom-error')
+
+
+
+
+
+module.exports = {
+    BadRequest,
+    CustomAPIError,
+    UnauthenticatedError
+}
